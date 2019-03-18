a97fb159b090e1bb602b49d49899936fb30c1df3
Tomas <47321445+tomasprogrammer2020@users.noreply.github.com>
2019-03-18 12:21:25 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int numero;
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+    int numero2;
+    printf("Ingrese otro numero: ");
+    scanf("%d", &numero2);
+int suma = numero + numero2;
+    printf("La suma es: %d", suma);
+    return 0;
+}
